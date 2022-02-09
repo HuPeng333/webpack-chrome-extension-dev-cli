e94869ef3de0dda630361d9c53dba77e6f46a444
HuPeng333 <2237803016@qq.com>
2022-02-09 16:50:22 +0800

    fix: 修复了重命名失败的bug

--- index.js
+++ index.js
@@ -7,18 +7,21 @@ process.stdin.resume()
 process.stdin.setEncoding('utf-8')
 process.stdout.write('project name: ')
 process.stdin.on('data', (data) => {
-  shell.exec('git clone https://github.com/HuPeng333/webpack-chrome-extension-dev-template', () => {
-    const BASE_URL = path.resolve(__dirname, data).trim()
-    fs.renameSync(path.resolve(__dirname, 'webpack-chrome-extension-dev-template'), BASE_URL)
+  console.log('downloading...')
+  shell.exec('git clone https://github.com/HuPeng333/webpack-chrome-extension-dev-template -b master', () => {
+    const curUrl = process.cwd()
+    const BASE_URL = path.resolve(curUrl, data).trim()
+    fs.renameSync(path.resolve(curUrl, 'webpack-chrome-extension-dev-template'), BASE_URL)
     deleteFolder(path.resolve(BASE_URL, '.git'))
     shell.exec(`git init ${BASE_URL}`)
+    console.log('download success')
     process.stdin.pause()
   })
 })
 
 function deleteFolder(path) {
   let files = [];
-  if( fs.existsSync(path) ) {
+  if(fs.existsSync(path) ) {
     files = fs.readdirSync(path);
     files.forEach(function(file,index){
       let curPath = path + "/" + file;
